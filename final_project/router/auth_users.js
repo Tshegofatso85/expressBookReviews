0b30f33d6635f7e4dd9923539fb9d840b8fc6e3c
Tshegofatso85 <tshegofatsokgokong19@gmail.com>
2025-07-25 05:14:02 -0400

    adding review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -33,7 +33,17 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  const review = req.query.review;
+  const username = req.session.authorization?.username;
+  
+  if(!book) return res.status(404).json({message: `Number ${isbn} book not found`});
+  if(!review) return res.status(400).json({message: "Review query is required"});
+  if(!username) return res.status(401).json({ message: "Unauthorized. Please log in first." });
+
+  book.reviews[username] = review;
+  return res.status(200).json({ message: "Review successfully added or updated", reviews: book.reviews });
 });
 
 module.exports.authenticated = regd_users;
